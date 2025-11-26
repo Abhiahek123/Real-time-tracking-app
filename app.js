0fd05c674fd9e470b1e6e6e3e937d4840d53a8c0
Abhishek Singh <rajputabhishek9886@gmail.com>
2025-11-26 09:04:57 +0530

    change html to ejs

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 // ejs setup
-
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
+
 io.on('connection', function (socket)  {
     socket.on('send-location', function (data) {
         io.emit("receive-location", {id: socket.id, ...data});
@@ -26,4 +26,6 @@ app.get("/", function (req, res) {
     res.render("index");
 });
 
-server.listen(3000);
+// ← CHANGE: use Render's PORT env variable (fallback to 3000 locally)
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
